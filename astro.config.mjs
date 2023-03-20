89a25f763498af2d044938ecb543a128791701bf
Matthew Carrano <mc25573@gmail.com>
2023-03-19 21:04:34 -0400

    finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    drafts: true,
+    drafts: true
   },
+  integrations: [preact()]
 });
